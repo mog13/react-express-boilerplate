4a8ca505fcd89d3066b7b077d6d74213e59b3cbf
Morgan Owen <mog13@live.com>
2020-03-28 15:21:02 +0000

    make sure react-router will work when deployed

--- server/index.js
+++ server/index.js
@@ -1,23 +1,21 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 
 const app = express();
-const port = 1337;
+const port = process.env.PORT || 1337;
 
 app.use(bodyParser.json());
-app.use(express.static('../build'));
-
-//THIS IS NOT A GOOD IDEA AND IS HERE TEMPORARILY
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
-});
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/api/',(req,res)=>{
 	res.send('this is the api root');
+});
+
+app.get('*', (req,res) =>{
+	res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 app.listen(port, () => console.log(`App listening on ${port}`));
